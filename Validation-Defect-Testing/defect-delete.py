93763277e5339a2f11879f6c1d8f425549af54de
SyedMuhammadFaheem <syed.faheem.official@gmail.com>
2023-05-11 23:07:35 +0500

    fixed bugs and issues

--- Validation-Defect-Testing/defect-delete.py
+++ Validation-Defect-Testing/defect-delete.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 
 def wait():
 	time.sleep(2)
-driver.get("http://localhost:8080/DBMSI/admin.php")
+driver.get("http://localhost:8080/DBMSI/deleteDoc.php")
 check=True
 driver.maximize_window()
 driver.implicitly_wait(5)
@@ -15,10 +15,18 @@ try:
     emailInputField.send_keys('ishaq.siddiqui1401@gmail.com')
     wait()
     passInputField= driver.find_element(By.CSS_SELECTOR,"input[name='password']")
-    passInputField.send_keys('123')
+    passInputField.send_keys('isaac_1401')
     submitBtn=driver.find_element(By.ID,"login")
     submitBtn.send_keys('\n')
+
+    driver.get("http://localhost:8080/DBMSI/deleteDoc.php")
+    idInputField= driver.find_element(By.ID,"form3Example1c")
+    idInputField.send_keys('9')
     wait()
+    
+    submitBtn=driver.find_element(By.ID,"delete-record")
+    submitBtn.send_keys('\n')
+    
     labelText = driver.find_element(By.ID,'error-delete').get_attribute('innerHTML')
     if 'Error' in labelText: 
         check=False
